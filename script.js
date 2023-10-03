ca67c6050c00bfe25b2cf6bed8150ceaaf7272da
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-03 16:29:59 +0600

    Implement simple css.

--- script.js
+++ script.js
@@ -8,11 +8,20 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+//   when guess false like 0 etc.
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number !!';
-  } else if (guess === secretNumber) {
+  } 
+//   when guess equal to secretNumber
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number.';
-  } else if (guess > secretNumber) {
+
+    document.querySelector('body').style.backgroundColor='green';
+    document.querySelector('.number').style.width='30rem';
+  } 
+//   when guess is too high
+  else if (guess > secretNumber) {
+    // for did not match the secret number score chance will be decrease.
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high !';
       score--;
@@ -22,7 +31,9 @@ document.querySelector('.check').addEventListener('click', () => {
         document.querySelector('.message').textContent='You lost the game.!';
         document.querySelector('.score').textContent=0;
     }
-  } else if (guess < secretNumber) {
+  } 
+//   when guess is too low 
+  else if (guess < secretNumber) {
     if(score>1){
         document.querySelector('.message').textContent='Too Low !';
         score--;
